6fdfa9d5b02bf18efe119a12c0511e329ecba4e2
Reece Withers <reece.withers2000@gmail.com>
2024-01-21 17:21:41 +1100

    Validation on request body completed for simple todo creation

--- src/backend/constants/messages.go
+++ src/backend/constants/messages.go
@@ -6,3 +6,7 @@ const USERNAME_TAKEN = "username is already in use"
 const INVALID_JWT = "invalid JWT in Access-Token header"
 const INVALID_CLAIMS = "invalid claims in Access-Token header"
 const UNAUTHORISED = "UNAUTHORISED"
+const EMPTY_FIELD = "field must not be empty"
+const DESCRIPTION_EMPTY = "description " + EMPTY_FIELD
+const INVALID_DATE = "field must be of the format YYYY-MM-DD and be today or later"
+const INVALID_TARGET_DATE = "targetDate " + INVALID_DATE
--- src/backend/service/simpleTodoService.go
+++ src/backend/service/simpleTodoService.go
@@ -3,9 +3,12 @@ package service
 import (
 	"encoding/json"
 	"fmt"
+	"main/constants"
 	"main/util"
 	"net/http"
-	"regexp"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -28,17 +31,52 @@ func HandleCreateSimpleTodo(writer http.ResponseWriter, req *http.Request, _ htt
 
 	if err := verifyCreateTodoRequestBody(todo); err != nil {
 		util.ThrowBadRequest(writer, err.Error())
+		return
 	}
-
 }
 
 func verifyCreateTodoRequestBody(todo SimpleTodo) error {
 	if len(todo.Description) == 0 {
-		return fmt.Errorf("description field must not be empty")
+		return fmt.Errorf(constants.DESCRIPTION_EMPTY)
 	}
-	if match, _ := regexp.MatchString("^\\d{4}-\\d{2}-\\d{2}$", todo.TargetDate); !match {
-		return fmt.Errorf("targetDate field must be of the format YYYY-MM-DD")
+
+	if _, err := time.Parse("2006-01-02", todo.TargetDate); err != nil {
+		fmt.Println(err)
+		return fmt.Errorf(constants.INVALID_TARGET_DATE)
+	}
+
+	if isDateBeforeNow(todo.TargetDate, time.Now().Format("2006-01-02")) {
+		return fmt.Errorf("")
 	}
 
 	return nil
+}
+
+func isDateBeforeNow(dateToCheck, now string) bool {
+	// No need to check for invalid numbers after splitting as time.Parse checked already
+	dateSplit := strings.Split(dateToCheck, "-")
+	year, _ := strconv.Atoi(dateSplit[0])
+	month, _ := strconv.Atoi(dateSplit[1])
+	day, _ := strconv.Atoi(dateSplit[2])
+
+	nowSplit := strings.Split(now, "-")
+	currYear, _ := strconv.Atoi(nowSplit[0])
+	currMonth, _ := strconv.Atoi(nowSplit[1])
+	currDay, _ := strconv.Atoi(nowSplit[2])
+
+	if year < currYear {
+		return true
+	}
+
+	if year == currYear {
+		if month < currMonth {
+			return true
+		} else if month == currMonth {
+			return day < currDay
+		} else {
+			return false
+		}
+	}
+
+	return false
 }
